6a2ef73b8799ed23f3480b53067926873974fb40
vitorrafaelm <vitor.rafael1518@gmail.com>
2021-06-09 10:14:39 -0300

    challenge done

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,22 @@
 import { v4 as uuidV4 } from "uuid";
 
 class User {
-  // Complete aqui
+  id: string;
+
+  name: string;
+
+  admin: boolean;
+
+  email: string;
+
+  created_at: Date;
+
+  updated_at: Date;
+
+  constructor() {
+    this.admin = false;
+    this.id = uuidV4();
+  }
 }
 
 export { User };
